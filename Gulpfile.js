116c290f22db958b1383f9ad18e0171a6c42000d
manidf <mannuel.ferreira@gmail.com>
2016-06-19 00:44:44 +0200

    bundling application with browserify

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,26 @@
 var gulp = require('gulp');
 var LiveServer = require('gulp-live-server');
 var browserSync = require('browser-sync');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var reactify = require('reactify');
+
 
 gulp.task('live-server', function() {
 	var server = new LiveServer('server/main.js');
 	server.start();
+});
+
+gulp.task('bundle', function() { //transform JSX to JS
+	return browserify({
+		// pass the starting point
+		entries: 'app/main.jsx',
+		debug: true,
+	})
+	.transform(reactify)
+	.bundle()
+	.pipe(source('app.js'))
+	.pipe(gulp.dest('./.tmp'));
 });
 
 gulp.task('serve', ['live-server'], function() {
