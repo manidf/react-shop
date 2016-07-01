acbb5e200a0668d0a1cf781b8d97a6ba8aceb1df
manidf <mannuel.ferreira@gmail.com>
2016-07-01 20:06:09 +0200

    added eslint

--- app/components/StockItemAddItem.jsx
+++ app/components/StockItemAddItem.jsx
@@ -2,11 +2,21 @@ var React = require('react');
 var ReactAddons = require('react-addons-update');
 
 module.exports = React.createClass({
+	getInitialSate:function() {
+		return { input: "" };
+	},
+	handleInputName:function(e) {
+		this.setState({input: e.target.value});
+	},
+	addItem:function(e) {
+		e.preventDefault;
+		console.log("Adding Item", this.state.input);
+	},
 	render:function() {
 		return (
 			<div className="StockAddItem">
-				<form action="">
-    			<input type="text" />
+				<form onSubmit={this.addItem}>
+    			<input value={this.state.input} onChange={this.handleInputName} />
 					<button> Add Item </button>
     		</form>
    		</div>
